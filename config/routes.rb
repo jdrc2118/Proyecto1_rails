1823a588d0ec8c6e4176cb9f14cbaa6b1b586b10
Juan Rojas <jdrc2118@gmail.com>
2021-10-19 18:39:52 +0000

    create controller static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/basics'
+  get 'static_pages/ruby'
+  get 'static_pages/git'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
